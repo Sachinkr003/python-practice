3f24a28c671f11397b23b473b5714e5becd6dace
Sachinkr003 <165494702+Sachinkr003@users.noreply.github.com>
2026-09-24 13:15:18 +0530

    Add files via upload

---
+++ Typcasting.py
@@ -0,0 +1,12 @@
+# In this process of converting a variable from one data type to another 
+#      srt(), int(), float(), boolean().
+
+name = "sachin"
+age = 23
+gpa = 4.8
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_student))
