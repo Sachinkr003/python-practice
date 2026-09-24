df4706a1bc80f5cd450b6e0225129c1921717d47
Sachinkr003 <165494702+Sachinkr003@users.noreply.github.com>
2026-09-24 13:14:52 +0530

    Add files via upload

---
+++ User.input.py
@@ -0,0 +1,10 @@
+#input() = A function that promnts the user do input data returns the entered data as a string.
+#           returns the entered data as a string.
+
+
+name = input("What is your name?:  ")
+age = input("How old are you?:  ")
+
+print(f"Hello {name}!")
+print(f"How are you?")
+print(f"You are {age} years old")
