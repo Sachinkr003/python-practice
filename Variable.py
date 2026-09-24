c9e9381d8027aec14574f547d367f97ad5205ba2
Sachinkr003 <165494702+Sachinkr003@users.noreply.github.com>
2026-09-24 13:14:27 +0530

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,41 @@
+#   variable = a conteiner for a value (string, integer, float, boolean)
+#              A variable behaves as if it was the value it continous.
+
+#strings
+
+first_name = "sachin"
+food = "pizza"
+email = "sachinkumarssss07"
+
+print(f"Hello {first_name}")
+print(f"I want to eat {food}")
+print(f"My email is {email}")
+
+#Integer
+
+age = 23
+quantity = 10
+num_of_students = 60
+
+print(f"You are {age} years old")
+print(f"Give me {quantity} each")
+print(f"students in the {num_of_students} class")
+
+#float
+
+price = 11.99 
+gpa = 4.5
+distance = 6.7
+
+print(f" The price is {price}")
+print(f" Your gpa is {gpa}")
+print(f" You run {distance}")
+
+#  Boolean
+
+is_student = False
+
+if is_student:
+      print("you are a student")
+else:
+      print("you are a NOT student")
