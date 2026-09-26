7520c872e0b06cbfcca8446d27cbec2e25eb6898
Sachin <165494702+Sachinkr003@users.noreply.github.com>
2026-09-26 23:28:14 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,23 @@
+operator = input("Enter an operator (+ - * /): ")
+num1 = float( input("Enter the 1st number: "))
+num2 = float( input("Enter the 2nd number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(result)
+elif operator == "-":
+    result = num1 - num2
+    print(result)
+elif operator == "*":
+    result = num1 * num2
+    print(result)
+elif operator == "/":
+    result = num1 / num2
+    print(result)
+else:
+    print(f"{operator} is not a valit operator")
+
+
+#If you get round function decimal output
+#then, 
+# print(round(result))
